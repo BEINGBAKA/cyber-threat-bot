c3c1d7b5fe5f3d0ea224704cd843038d1a839546
Ishika Mittal <i.ishikamittal03@gmail.com>
2025-06-25 03:09:08 +0530

    memory reduction solutions

--- backend/vector_db/chroma_store.py
+++ backend/vector_db/chroma_store.py
@@ -1,6 +1,6 @@
-from langchain.vectorstores import FAISS
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.docstore.document import Document
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
 
 embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 vectordb = None  # Will be initialized later
